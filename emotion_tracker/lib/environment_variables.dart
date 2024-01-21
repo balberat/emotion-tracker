c1045a37a8528a6623965c1c9d07ad9845d082b0
balberat <101412991+balberat@users.noreply.github.com>
2024-01-21 17:19:13 +0300

    Update environment_variables.dart

--- emotion_tracker/lib/environment_variables.dart
+++ emotion_tracker/lib/environment_variables.dart
@@ -4,6 +4,8 @@ class Environment {}
 
 const String APP_TITLE = "Emotion Tracker";
 
-const String USER_TOKEN = "Token token=d0e53a972f3eaa7dcd8ff89b946f0f68";
+const String USER_TOKEN = "d0e53a972f3eaa7dcd8ff89b946f0f68";
 
-const String CALLBACK_URL = "https://favqs.com/api/quotes/";
+const String QUOTE_CALLBACK_URL = "https://favqs.com/api/quotes/";
+const String IMAGE_CALLBACK_URL =
+    "https://en.wikipedia.org/w/api.php?action=query&generator=search&gsrlimit=1&prop=pageimages%7Cextracts&pithumbsize=400&gsrsearch=";
