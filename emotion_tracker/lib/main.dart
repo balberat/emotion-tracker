6bdfe6ced8b04a71295ccc4ca1704f7db635691c
balberat <101412991+balberat@users.noreply.github.com>
2024-01-21 17:16:40 +0300

    Update main.dart

--- emotion_tracker/lib/main.dart
+++ emotion_tracker/lib/main.dart
@@ -1,8 +1,19 @@
+import 'dart:async';
+
+import 'package:emotion_tracker/application/history_bloc/history_bloc.dart';
+import 'package:emotion_tracker/environment_variables.dart' as env;
+import 'package:emotion_tracker/injection.dart';
 import 'package:emotion_tracker/presentation/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runZonedGuarded(() async {
+    await setupInjection();
+    runApp(const MyApp());
+  }, (error, stack) {});
 }
 
 class MyApp extends StatelessWidget {
@@ -10,13 +21,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Emotion Tracher',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => getIt<HistoryBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: env.APP_TITLE,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const MainPage(),
       ),
-      home: const MainPage(),
     );
   }
 }
