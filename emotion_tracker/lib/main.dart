b01159a0cd80c220332318db4c2c56d6bbdf85ad
balberat <101412991+balberat@users.noreply.github.com>
2024-01-22 15:07:49 +0300

    add notification settings to main

--- emotion_tracker/lib/main.dart
+++ emotion_tracker/lib/main.dart
@@ -3,7 +3,10 @@ import 'dart:async';
 import 'package:emotion_tracker/application/history_bloc/history_bloc.dart';
 import 'package:emotion_tracker/environment_variables.dart' as env;
 import 'package:emotion_tracker/injection.dart';
+import 'package:emotion_tracker/presentation/history_page.dart';
 import 'package:emotion_tracker/presentation/main_page.dart';
+import 'package:emotion_tracker/presentation/quote_display_page.dart';
+import 'package:emotion_tracker/service/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -11,12 +14,15 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runZonedGuarded(() async {
+    await NotificationService.inititializeNotification();
     await setupInjection();
     runApp(const MyApp());
   }, (error, stack) {});
 }
 
 class MyApp extends StatelessWidget {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
   const MyApp({super.key});
 
   @override
@@ -30,11 +36,17 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: env.APP_TITLE,
         debugShowCheckedModeBanner: false,
+        navigatorKey: navigatorKey,
+        routes: {
+          '/': (context) => const MainPage(),
+          '/history': (context) => const HistoryPage(),
+          '/quote': (context) => const QuoteDisplayPage(),
+        },
+        initialRoute: '/',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MainPage(),
       ),
     );
   }
